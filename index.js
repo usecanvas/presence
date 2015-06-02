ef8186b025effc08df183430d3b97d7fd73aa779
Jonathan Clem <jotclem@gmail.com>
2015-06-02 10:35:44 -0400

    Add basic echo ws server

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 'use strict';
 
+const http     = require('http');
 const koa      = require('koa');
-const ws       = require('ws');
 const teamster = require('teamster');
+const ws       = require('ws');
 const app      = koa();
 
-teamster.runServer(app.callback(), {
+teamster.run(runServer, {
   fork      : process.env.NODE_ENV === 'production',
   numWorkers: parseInt(process.env.NUM_WORKERS, 10) || 1,
-  port      : parseInt(process.env.PORT, 10) || 5000,
 });
+
+function runServer() {
+  const port = parseInt(process.env.PORT, 10) || 5000;
+  http.createServer(app.callback()).listen(port, () => runWSServer(this));
+}
+
+function runWSServer(httpServer) {
+  const wsServer = new ws.Server({ server: httpServer })
+  wsServer.on('connection', onConnection);
+}
+
+function onConnection(client) {
+  client.on('message', onMessage);
+}
+
+function onMessage(message) {
+  this.send(message);
+}
