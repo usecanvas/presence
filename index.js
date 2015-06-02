0c488cb8d13d17eff49cd69c34a0fcba5c8471d6
Jonathan Clem <jotclem@gmail.com>
2015-06-02 11:18:07 -0400

    Add basic (permanent) presence implementation

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const http     = require('http');
 const isUUID   = require('validator').isUUID;
 const koa      = require('koa');
 const logfmt   = require('logfmt');
+const redis    = require('redis');
 const teamster = require('teamster');
+const url      = require('url');
 const ws       = require('ws');
 const app      = koa();
+const redisCmd = createRedisClient();
 
 teamster.run(runServer, {
   fork      : process.env.NODE_ENV === 'production',
@@ -28,34 +31,74 @@ function onConnection(client) {
 }
 
 function dispatchMessage(message) {
+  const client = this;
+
   try {
     message = JSON.parse(message);
   } catch(err) {
-    handleUnparsableMessage(this, message);
+    handleUnparsableMessage(client, message);
     return;
   }
 
   if (!isUUID(message.space, '4')) {
-    handleNonUUIDSpace(this, message);
+    handleNonUUIDSpace(client, message);
+    return;
+  }
+
+  if (!message.identity) {
+    handleNoIdentityProvided(client, message);
     return;
   }
 
   switch (message.action) {
     case 'join':
       logfmt.log({ action: 'join' });
-      sendMessage(client, { action: 'join' });
+
+      redisCmd.multi()
+        .sadd(message.space, message.identity)
+        .smembers(message.space)
+        .exec((err, replies) => {
+          if (err) {
+            handleRedisError(client, err);
+            return;
+          }
+
+          sendMessage(client, { action: 'join', members: replies[1] });
+        });
+
       break;
     case 'ping':
       logfmt.log({ action: 'ping' });
       sendMessage(client, { action: 'ping' });
+      redisCmd.sadd(message.space, message.identity);
       break;
     case 'leave':
       logfmt.log({ action: 'leave' });
       sendMessage(client, { action: 'leave' });
+      redisCmd.srem(message.space, message.identity);
       break;
     default:
-      handleUnrecognizedAction(this, message);
+      handleUnrecognizedAction(client, message);
   }
+}
+
+function createRedisClient() {
+  const redisURL = url.parse(process.env.REDIS_URL);
+  const password = url.auth ? url.auth.split(':')[1] : null;
+  return redis.createClient(redisURL.port, redisURL.hostname,
+    { auth_pass: password });
+}
+
+function handleRedisError(client, error) {
+  logfmt.error(error);
+  sendMessage(client,
+    { errors: [{ detail: 'An unexpected error occurred.' }]});
+}
+
+function handleNoIdentityProvided(client/*, message */) {
+  logfmt.log({ error: 'No identity provided.' });
+  sendMessage(client,
+    { errors: [{ detail: 'No "identity" param provided.' }]});
 }
 
 function handleNonUUIDSpace(client, message) {
